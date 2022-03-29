81547c43d9647a8c7d4a2a70e15ac138480e0b29
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-03-29 21:18:56 +0200

    login view componentization

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,70 +21,27 @@
         class="w-2/5"
       />
 
-      <form
-        class="w-2/5 flex flex-col space-y-14 text-ligthRose"
-      >
-        <div
-          class="flex flex-col space-y-4"
-        >
-          <label 
-            for=""
-            class="text-2xl"
-          >
-            Correo Electr&oacute;nico
-          </label>
-          <input 
-            type="email" 
-            required 
-            class="text-xl bg-transparent border-b border-ligthRose outline-none"
-          />
-        </div>
-
-        <div
-          class="flex flex-col space-y-4"
-        >
-          <label 
-            for=""
-            class="text-2xl"
-          >
-            Contrase&ntilde;a
-          </label>
-          <input 
-            type="password" 
-            required 
-            class="text-xl bg-transparent border-b border-ligthRose outline-none"
-          />
-        </div>
-
-        <div
-          class="flex flex-col items-center space-y-2"
-        >
+      <FormLogin>
+        <slot>
           <button 
             type="button" 
             class="self-center btn outline outline-ligthRose text-ligthRose w-5/6 text-4xl rounded-full py-2"
           >
             Iniciar Sesi&oacute;n
           </button>
-
-          <p
-            class="text-ligthRose"
-          >
-            ¿No tienes una cuenta?
-            <router-link 
-              class="text-lg font-bold" 
-              :to="{ name: 'register' }"
-            >
-              Reg&iacute;strate
-            </router-link>
-          </p>
-        </div>
-      </form>
+        </slot>
+      </FormLogin>      
     </article>
   </main>
 </template>
 
 <script>
+import FormLogin from '../components/FormLogin.vue'
+
 export default {
   name: "Login",
+  components: {
+    FormLogin,
+  }
 };
 </script>
