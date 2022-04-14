fcc58e13218da03ababa5b7d5539c64b604a5642
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-04-14 22:00:13 +0200

    init add-experience view

---
+++ src/components/RateExperience.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="flex space-x-4">
+         <span
+            class="text-xl"
+        >Califica tu experiencia</span>
+<!--         <label for="plus1">
+            <svg 
+            class="w-8 self-center" 
+            :class="[
+                checked ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+        </label>
+        <input type="radio" name="rated" id="plus1" class="hidden" value="1"> -->
+
+        <svg 
+            class="w-8 self-center" 
+            :class="[
+                fillIcon ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            @click="selectedIcon"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+        <svg 
+            class="w-8 self-center" 
+            :class="[
+                fillIcon ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            @click="selectedIcon"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+        <svg 
+            class="w-8 self-center fill-transparent stroke-cosmicLatte" 
+            :class="[
+                fillIcon ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            @click="selectedIcon"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+        <svg 
+            class="w-8 self-center fill-transparent stroke-cosmicLatte" 
+            :class="[
+                fillIcon ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            @click="selectedIcon"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+        <svg 
+            class="w-8 self-center fill-transparent stroke-cosmicLatte" 
+            :class="[
+                fillIcon ? 'fill-cosmicLatte' : 'fill-transparent stroke-cosmicLatte'
+            ]"
+            @click="selectedIcon"
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RateExperience",
+    data() {
+        return {
+            fillIcon: false,
+        };
+    },
+    methods: {
+        selectedIcon() {
+            return this.fillIcon = !this.fillIcon;
+        }
+    }
+}
+</script>
--- src/views/AddExperience.vue
+++ src/views/AddExperience.vue
@@ -1,14 +1,75 @@
 <template>
   <Header />
+  <form 
+    class="w-full h-screen text-cosmicLatte flex flex-col justify-center items-center pb-56 space-y-6"
+  >
+    <div
+      class="flex flex-col space-y-4"
+    >
+      <label 
+        class="text-lg"
+        for="experience"
+      >
+        Mi experiencia
+      </label>
+      <textarea 
+        id="experience" cols="36" rows="4"
+        class="text-lg px-1 bg-transparent border-b border-cosmicLatte outline-none resize-none"
+      />
+    </div>
+
+    <label for="image"
+      class="w-3/12 text-center py-2 outline outline-cosmicLatte rounded-full cursor-pointer"
+    >
+      A&ntilde;ade una imagen de tu experiencia
+    </label>
+    <input @change="selectedImage" type="file" accept="image/*" size="60" class="">
+    <img id="selectedImage" alt="">
+    
+    <RateExperience />
+
+    <button 
+      type="button"
+      class="self-center btn bg-cosmicLatte text-ruby w-3/12 rounded-full py-2 text-lg"
+    >
+      Conservar experiencia
+    </button>
+  </form>
 </template>
 
 <script>
 import Header from "../components/Header.vue"
+import RateExperience from "../components/RateExperience.vue"
 
 export default {
   name: 'AddExperience',
   components: {
     Header,
+    RateExperience,
+  },
+  data() {
+    return {
+      // selectedImage: '',
+    };
+  },
+  /* watcher: {
+    image() {
+      if (this.image.length) {
+        return this.selectedImage = window.URL.createObjectURL(this.image);
+      }
+    }
+  } */
+  methods: {
+    /* showImage() {
+      const file = document.getElementById('image');
+      let img = document.getElementById('selectedImage');
+
+      console.log(file);
+      file.addEventListener("change", () => {
+        const urlImage = URL.createObjectURL(file);
+        img.src = urlImage;
+      })
+    } */
   }
 }
 </script>
