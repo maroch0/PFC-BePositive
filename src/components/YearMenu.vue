6ea1940d2d5390b193454d2703ebe4d29d2b66e7
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-09 20:42:19 +0200

    componetized timeline layout

---
+++ src/components/YearMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-cosmicLatte">
+    <!-- left -->
+    <div class="flex flex-row-reverse md:contents">
+      <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
+        <h3 class="text-lg">2022</h3>
+      </div>
+      <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+        <div class="h-full w-6 flex items-center justify-center">
+          <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
+        </div>
+      </div>
+    </div>
+    <!-- left -->
+    <div class="flex flex-row-reverse md:contents">
+      <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
+        <h3 class="text-lg">2021</h3>
+      </div>
+      <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+        <div class="h-full w-6 flex items-center justify-center">
+          <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
+        </div>
+      </div>
+    </div>
+    <!-- left -->
+    <div class="flex flex-row-reverse md:contents">
+      <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
+        <h3 class="font-semibold text-2xl">2020</h3>
+      </div>
+      <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+        <div class="h-full w-6 flex items-center justify-center">
+          <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
+        </div>
+        <!-- dot -->
+        <div
+          class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
+        ></div>
+      </div>
+    </div>
+    <!-- left -->
+    <div class="flex flex-row-reverse md:contents">
+      <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
+        <h3 class="text-lg">2019</h3>
+      </div>
+      <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+        <div class="h-full w-6 flex items-center justify-center">
+          <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "YearMenu"
+};
+</script>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -3,219 +3,26 @@
 
   <section class="flex h-5/6 overflow-hidden">
     <aside class="w-2/6 flex justify-center items-center">
-      <div
-        class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-cosmicLatte"
-      >
-      <!-- left -->
-        <div class="flex flex-row-reverse md:contents">
-          <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-            <h3 class="text-lg">2022</h3>
-          </div>
-          <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div class="h-full w-6 flex items-center justify-center">
-              <div
-                class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-              ></div>
-            </div>
-          </div>
-        </div>
-        <!-- left -->
-        <div class="flex flex-row-reverse md:contents">
-          <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-            <h3 class="text-lg">2021</h3>
-          </div>
-          <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div class="h-full w-6 flex items-center justify-center">
-              <div
-                class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-              ></div>
-            </div>
-          </div>
-        </div>
-        <!-- left -->
-        <div class="flex flex-row-reverse md:contents">
-          <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-            <h3 class="font-semibold text-2xl">2020</h3>
-          </div>
-          <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div class="h-full w-6 flex items-center justify-center">
-              <div
-                class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-              ></div>
-            </div>
-            <!-- dot -->
-            <div
-              class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-            ></div>
-          </div>
-        </div>
-        <!-- left -->
-        <div class="flex flex-row-reverse md:contents">
-          <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-            <h3 class="text-lg">2019</h3>
-          </div>
-          <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-            <div class="h-full w-6 flex items-center justify-center">
-              <div
-                class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-              ></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <YearMenu />
     </aside>
 
     <article class="w-4/6 h-full overflow-auto">
-      <div class="container">
-        <div
-          class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-cosmicLatte"
-        >
-          <!-- left -->
-          <div class="flex flex-row-reverse md:contents">
-            <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-              <h3 class="italic text-lg mb-1">10 de Agosto de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                Me siento genial.
-              </p>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-          </div>
-          <!-- right -->
-          <div class="flex md:contents">
-            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-            <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-              <h3 class="italic text-lg mb-1">14 de Junio de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                Primeras palabras de Mateo.
-              </p>
-            </div>
-          </div>
-          <!-- left -->
-          <div class="flex flex-row-reverse md:contents">
-            <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-              <h3 class="italic text-lg mb-1">27 de Mayo de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                He descubierto un sitio muy bonito en la montaña.
-              </p>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-          </div>
-          <!-- right -->
-          <div class="flex md:contents">
-            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-            <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-              <h3 class="italic text-lg mb-1">19 de Abril de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                Me han ascendido en el trabajo.
-              </p>
-            </div>
-          </div>
-          <!-- left -->
-          <div class="flex flex-row-reverse md:contents">
-            <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-              <h3 class="italic text-lg mb-1">1 de Marzo 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                He hecho un nuevo amigo.
-              </p>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-          </div>
-          <!-- right -->
-          <div class="flex md:contents">
-            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-            <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-              <h3 class="italic text-lg mb-1">13 de Febrero de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                Fiesta de empresa.
-              </p>
-            </div>
-          </div>
-          <!-- left -->
-          <div class="flex flex-row-reverse md:contents">
-            <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-              <h3 class="italic text-lg mb-1">1 de Enero de 2020</h3>
-              <p class="leading-tight text-xl text-justify">
-                Un nuevo año me espera.
-              </p>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div
-                  class="h-full w-0.5 bg-cosmicLatte pointer-events-none"
-                ></div>
-              </div>
-              <div
-                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-cosmicLatte shadow"
-              ></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TimelineItem />
     </article>
   </section>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import TimelineItem from '../components/TimelineItem.vue';
+import YearMenu from "../components/YearMenu.vue"
 
 export default {
   name: "Timeline",
   components: {
     Header,
+    YearMenu,
+    TimelineItem,
   },
 };
 </script>
