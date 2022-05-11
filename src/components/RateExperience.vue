69159a77e56297ff51e6266ad7a37bc3509af82c
maroch0 <mr.romaguera.ch@gmail.com>
2022-05-11 20:36:18 +0200

    fix responsive design to rating

--- src/components/RateExperience.vue
+++ src/components/RateExperience.vue
@@ -8,19 +8,19 @@
         
         <div class="rate flex flex-row-reverse justify-center items-center">
             <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1">1 stars</label>
+            <label class="w-8 md:w-7" for="star1">1 star</label>
     
             <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2">2 stars</label>
+            <label class="w-8 md:w-7" for="star2">2 stars</label>
     
             <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3">3 stars</label>
+            <label class="w-8 md:w-7" for="star3">3 stars</label>
     
             <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4">4 stars</label>
+            <label class="w-8 md:w-7" for="star4">4 stars</label>
     
             <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5">5 star</label>
+            <label class="w-8 md:w-7" for="star5">5 star</label>
         </div>
     </div>
 </template>
@@ -43,7 +43,7 @@ export default {
 }
 .rate:not(:checked) > label {
     float:right;
-    width:.94em;
+    /*width:.85em;*/
     overflow:hidden;
     white-space:nowrap;
     cursor:pointer;
