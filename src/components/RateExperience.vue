1d6142c41db6105a6afb6d8388cc11df55b97e43
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-11 23:01:27 +0200

    some changes

--- src/components/RateExperience.vue
+++ src/components/RateExperience.vue
@@ -8,19 +8,19 @@
         
         <div class="rate flex flex-row-reverse justify-center items-center">
             <input type="radio" id="star1" name="rate" value="1" />
-            <label class="w-8 md:w-7" for="star1">1 star</label>
+            <label class="md:w-7" for="star1">1 star</label>
     
             <input type="radio" id="star2" name="rate" value="2" />
-            <label class="w-8 md:w-7" for="star2">2 stars</label>
+            <label class="md:w-7" for="star2">2 stars</label>
     
             <input type="radio" id="star3" name="rate" value="3" />
-            <label class="w-8 md:w-7" for="star3">3 stars</label>
+            <label class="md:w-7" for="star3">3 stars</label>
     
             <input type="radio" id="star4" name="rate" value="4" />
-            <label class="w-8 md:w-7" for="star4">4 stars</label>
+            <label class="md:w-7" for="star4">4 stars</label>
     
             <input type="radio" id="star5" name="rate" value="5" />
-            <label class="w-8 md:w-7" for="star5">5 star</label>
+            <label class="md:w-7" for="star5">5 star</label>
         </div>
     </div>
 </template>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -3,7 +3,7 @@
 
   <section class="flex h-5/6 overflow-hidden">
     <aside class="w-2/6 flex justify-center items-center">
-      <YearMenu />
+      <TestYearMenu />
     </aside>
 
     <article class="w-4/6 h-full overflow-auto">
@@ -15,13 +15,13 @@
 <script>
 import Header from "../components/Header.vue";
 import TimelineItem from '../components/TimelineItem.vue';
-import YearMenu from "../components/YearMenu.vue"
+import TestYearMenu from "../components/TestYearMenu.vue"
 
 export default {
   name: "Timeline",
   components: {
     Header,
-    YearMenu,
+    TestYearMenu,
     TimelineItem,
   },
 };
