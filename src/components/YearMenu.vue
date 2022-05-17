c5f975eec8162fde19c191f27b56cc546d4a025f
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-17 19:02:57 +0200

    fix apply styles to current year when page it's loaded

--- src/components/YearMenu.vue
+++ src/components/YearMenu.vue
@@ -1,43 +1,50 @@
 <template>
-<div class="flex flex-col relative border-r border-cosmicLatte dark:border-cosmicLatte">
-    <!-- Raya -->                
-    <div class="mb-4 mr-6">
-        <time class="w-full text-xl text-cosmicLatte dark:text-cosmicLatte">2022</time>
-    </div>
-
-    <!-- Raya -->                
-    <div class="mb-4 mr-6">
-        <time class="w-full text-xl text-cosmicLatte dark:text-cosmicLatte">2021</time>
-    </div>
-
-    <!-- Raya -->                
-    <div class="mb-4 mr-6">
+  <div
+    class="flex flex-col relative border-r border-cosmicLatte dark:border-cosmicLatte"
+  >
+    <div v-for="(year, index) in years" :key="index">
+      <!-- Raya -->
+      <div class="mb-4 mr-6">
         <!-- Bolita -->
-        <div class="absolute w-4 h-4 bg-cosmicLatte rounded-full mt-2 -right-2 border border-cosmicLatte dark:border-cosmicLatte dark:bg-cosmicLatte"></div>
-        <time class="w-full text-3xl text-cosmicLatte dark:text-cosmicLatte">2020</time>
+        <div v-if="selected && yearSelect === year">
+          <div
+            class="absolute w-4 h-4 bg-cosmicLatte rounded-full mt-2 -right-2 border border-cosmicLatte dark:border-cosmicLatte dark:bg-cosmicLatte"
+          ></div>
+        </div>
+        <button type="button" class="w-full" @click="yearSelected(index)">
+          <p
+            class="text-xl text-cosmicLatte dark:text-cosmicLatte"
+            :class="{
+              'text-3xl': yearSelect === year
+            }"
+          >
+            {{ year }}
+          </p>
+        </button>
+      </div>
     </div>
-
-    <!-- Raya -->                
-    <div class="mb-4 mr-6">
-        <time class="w-full text-xl text-cosmicLatte dark:text-cosmicLatte">2019</time>
-    </div>
-
-    <!-- Raya -->                
-    <div class="mb-4 mr-6">
-       <time class="w-full text-xl text-cosmicLatte dark:text-cosmicLatte">2018</time>
-    </div>
-</div>
-    
+  </div>
 </template>
 
 <script>
 export default {
-  name: "YearMenu",
+  name: 'YearMenu',
   data() {
     return {
       years: [2022, 2021, 2020, 2019, 2018],
-      selected: false,
-    };
+      yearSelect: undefined,
+      selected: false
+    }
   },
-};
+  created() {
+    this.yearSelect = this.years[0]
+    this.selected = true
+  },
+  methods: {
+    yearSelected(index) {
+      this.yearSelect = this.years[index]
+      this.selected = true
+    }
+  }
+}
 </script>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -3,7 +3,7 @@
 
   <section class="flex h-5/6 overflow-hidden">
     <aside class="w-2/6 flex justify-center items-center">
-      <TestYearMenu />
+      <YearMenu />
     </aside>
 
     <article class="w-4/6 h-full overflow-auto">
@@ -15,13 +15,13 @@
 <script>
 import Header from "../components/Header.vue";
 import TimelineItem from '../components/TimelineItem.vue';
-import TestYearMenu from "../components/TestYearMenu.vue"
+import YearMenu from "../components/YearMenu.vue"
 
 export default {
   name: "Timeline",
   components: {
     Header,
-    TestYearMenu,
+    YearMenu,
     TimelineItem,
   },
 };
