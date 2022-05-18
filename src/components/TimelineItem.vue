b0dc289f2ee8b42b2adb70c77165896dd4d40268
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-18 19:59:58 +0200

    change hardcode html timeline to dynamic

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,190 +1,148 @@
 <template>
-  <div class="container">
-    <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-cosmicLatte">
-      <!-- left -->
-      <div class="flex flex-row-reverse md:contents">
-        <div class="col-start-1 col-end-5 p-4 rounded-xl ml-auto">
-          <h3 class="italic text-lg mb-1">10 de Agosto de 2020</h3>
-          <p class="leading-tight text-xl text-justify">Me siento genial.</p>
-        </div>
-        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-      </div>
-      <!-- right -->
-      <div class="flex md:contents">
-        <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-        <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-          <h3 class="italic text-lg mb-1">14 de Junio de 2020</h3>
-          <p class="leading-tight text-xl text-justify">
-            Primeras palabras de Mateo.
-          </p>
-        </div>
-      </div>
-      <!-- left -->
-      <div class="flex flex-row-reverse md:contents">
-        <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-          <h3 class="italic text-lg mb-1">27 de Mayo de 2020</h3>
-          <p class="leading-tight text-xl text-justify">
-            He descubierto un sitio muy bonito en la montaña.
-          </p>
-        </div>
-        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-      </div>
-      <!-- right -->
-      <div class="flex md:contents">
-        <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-        <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-          <h3 class="italic text-lg mb-1">19 de Abril de 2020</h3>
-          <p class="leading-tight text-xl text-justify">
-            Me han ascendido en el trabajo.
-          </p>
-        </div>
-      </div>
-      <!-- left -->
-      <div class="flex flex-row-reverse md:contents">
-        <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-          <h3 class="italic text-lg mb-1">1 de Marzo 2020</h3>
-          <p class="leading-tight text-xl text-justify">
-            He hecho un nuevo amigo.
-          </p>
-        </div>
-        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-      </div>
-      <!-- right -->
-      <div class="flex md:contents">
-        <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-        <div class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto">
-          <h3 class="italic text-lg mb-1">13 de Febrero de 2020</h3>
-          <p class="leading-tight text-xl text-justify">Fiesta de empresa.</p>
-        </div>
-      </div>
-      <!-- left -->
-      <div class="flex flex-row-reverse md:contents">
-        <div class="col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto">
-          <h3 class="italic text-lg mb-1">1 de Enero de 2020</h3>
-          <p class="leading-tight text-xl text-justify">
-            Un nuevo año me espera.
-          </p>
-        </div>
-        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-          <div class="h-full w-6 flex items-center justify-center">
-            <div class="h-full w-0.5 bg-cosmicLatte pointer-events-none"></div>
-          </div>
-          <div
-            class="
-              w-6
-              h-6
-              absolute
-              top-1/2
-              -mt-3
-              rounded-full
-              bg-cosmicLatte
-              shadow
-            "
-          ></div>
-        </div>
-      </div>
+  <section class="timeline">
+    <div
+      v-for="(item, i) in data"
+      :key="i"
+      :class='i % 2 ? "timeline-item left" : "timeline-item right"'
+    >
+      <article 
+        class="content text-cosmicLatte"
+        :class='i % 2 ? "flex flex-col justify-end items-end" : "flex flex-col justify-end items-start"'
+      >
+        <h2 class="italic font-thin text-lg mb-1">{{item.date}}</h2>
+        <p class="leading-tight text-xl text-justify">{{item.description}}</p>
+      </article>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+const experiences = [
+        {
+          date: "01-01-2020",
+          description: "Un nuevo año me espera."
+        },
+        {
+          date: "13-02-2020",
+          description: "Fiesta de empresa."
+        },
+        {
+          date: "01-03-2020",
+          description: "He hecho un nuevo amigo."
+        },
+        {
+          date: "19-04-2020",
+          description: "Me han ascendido en el trabajo."
+        },
+        {
+          date: "27-05-2020",
+          description: "He descubierto un sitio muy bonito en la montaña."
+        },
+        {
+          date: "14-06-2020",
+          description: "Primeras palabras de Mateo."
+        },
+        {
+          date: "24-07-2020",
+          description: "Me siento genial."
+        },
+        {
+          date: "08-08-2020",
+          description: "Celebramos 10 años de matrimonio"
+        },
+        {
+          date: "26-10-2020",
+          description: "Fin de semana de spa con las amigas"
+        },
+        {
+          date: "03-11-2020",
+          description: "Nos mudamos a la ciudad"
+        },
+      ];
+
 export default {
   name: "TimelineItem",
+  data() {
+    return {
+      data: experiences
+    };
+  },
 };
 </script>
+
+<style scoped>
+.timeline {
+  position: relative;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+/* barra vertical */
+.timeline::after {
+  content: "";
+  position: absolute;
+  width: 2px;
+  background-color: #FFF8E8;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  margin-left: -2px;
+}
+
+.timeline-item {
+  padding: 10px 80px;
+  position: relative;
+  width: 50%;
+}
+
+.timeline-item::after {
+  content: "";
+  position: absolute;
+  /* dot size */
+  width: 20px;
+  height: 20px;
+
+  right: -10.2px;
+  background-color: #FFF8E8;
+  top: 28px;
+  border-radius: 50%;
+  z-index: 1;
+}
+
+.left {
+  left: 0;
+}
+/* dot horizontal position */
+.right {
+  left: 50%;
+}
+
+.right::after {
+  left: -10px;
+}
+
+.content {
+  padding: 20px 30px;
+}
+
+/* Responsive on screens less than 600px wide */
+@media screen and (max-width: 600px) {
+  .timeline::after {
+    left: 31px;
+  }
+
+  .timeline-item {
+    width: 100%;
+    padding-left: 70px;
+    padding-right: 25px;
+  }
+
+  .left::after,
+  .right::after {
+    left: 21px;
+  }
+
+  .right {
+    left: 0%;
+  }
+}
+</style>
