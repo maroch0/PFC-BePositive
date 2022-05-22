c3c3071f66455c8ec7331f6016c69a040d767473
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-22 23:12:22 +0200

    changes in register view

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,109 +1,90 @@
 <template>
-  <form 
-    class="text-cosmicLatte grid grid-cols-2 justify-center gap-8 ml-28"
-  >
-    <div
-      class="w-3/4 flex flex-col space-y-12"
-    >
-      <div class="w-full flex flex-col">
-        <label 
-          for="name"
-          class="text-2xl md:text-xl"
-        >
-          Nombre
-        </label>
-        <input 
-          type="text" 
+  <form class="w-8/12 grid grid-cols-2 justify-center content-center gap-8">
+    <div class="w-full flex flex-col space-y-12 ml-28">
+      <div class="w-full flex flex-col space-y-2">
+        <label for="name" class="text-2xl md:text-xl"> Nombre </label>
+        <input
+          type="text"
           id="name"
-          required 
-          class="w-full text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
+          required
+          class="w-3/4 text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
         />
       </div>
 
-      <div class="w-full flex flex-col">
-        <label 
-          for="gender"
-          class="text-2xl md:text-xl"
+      <div class="w-full flex flex-col space-y-2">
+        <label for="gender" class="text-2xl md:text-xl"> G&eacute;nero </label>
+        <select
+          name="gender"
+          id="gender"
+          class="w-3/4 text-xl md:text-base bg-transparent border-b border-cosmicLatte outline-none"
         >
-          G&eacute;nero
-        </label>
-        <select 
-          name="gender" id="gender"
-          class="w-full text-xl md:text-base bg-transparent border-b border-cosmicLatte outline-none"
-        >
-          <option value="masculino">Masculino</option>
-          <option value="femenino">Femenino</option>
-          <option value="no-binario">No Binario</option>
+          <option value="male">Masculino</option>
+          <option value="female">Femenino</option>
+          <option value="non-binary">No Binario</option>
         </select>
       </div>
 
-      <div class="w-full flex flex-col">
-        <label 
-          for="email"
-          class="text-2xl md:text-xl"
-        >
+      <div class="w-full flex flex-col space-y-2">
+        <label for="email" class="text-2xl md:text-xl">
           Correo Electr&oacute;nico
         </label>
-        <input 
+        <input
           type="email"
           id="email"
-          required 
-          class="w-full text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
-        />    
+          required
+          class="w-3/4 text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
+        />
       </div>
     </div>
 
-    <div
-      class="w-3/4 flex flex-col space-y-12"
-    >
-      <div class="w-full flex flex-col">
-        <label 
-          for="lastname"
-          class="text-2xl md:text-xl"
-        >
-          Apellidos
-        </label>
-        <input 
-          type="text" 
+    <div class="w-full flex flex-col space-y-12">
+      <div class="w-full flex flex-col space-y-2">
+        <label for="lastname" class="text-2xl md:text-xl"> Apellidos </label>
+        <input
+          type="text"
           id="lastname"
-          required 
-          class="w-full text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
+          required
+          class="w-3/4 text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
         />
       </div>
 
-      <div class="w-full flex flex-col">
-        <label 
-          for="birthDate"
-          class="text-2xl md:text-xl"
-        >
+      <div class="w-full flex flex-col space-y-2">
+        <label for="birthDate" class="text-2xl md:text-xl">
           Fecha de nacimiento
         </label>
-        <input type="date" id="birthDate" name="trip-start"
-          value="2004-01-01"
-          class="w-full text-xl md:text-base bg-transparent border-b border-cosmicLatte outline-none"
+        <input
+          type="date"
+          id="birthDate"
+          name="trip-start"
+          class="w-3/4 text-xl md:text-base bg-transparent border-b border-cosmicLatte outline-none"
         />
       </div>
 
-      <div class="w-full flex flex-col">
-        <label 
-          for="pass"
-          class="text-2xl md:text-xl"
-        >
+      <div class="w-full flex flex-col space-y-2">
+        <label for="pass" class="text-2xl md:text-xl">
           Contrase&ntilde;a
         </label>
-        <input 
-          type="password" 
+        <input
+          type="password"
           id="pass"
-          required 
-          class="w-full text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
+          required
+          class="w-3/4 text-xl md:text-base px-1 bg-transparent border-b border-cosmicLatte outline-none"
         />
-      </div>          
+      </div>
+    </div>
+    <div class="w-full flex justify-center items-center col-span-2">
+      <button
+        type="button"
+        class="btn bg-cosmicLatte text-intenseRose self-center w-2/5 text-2xl rounded-full py-2 mr-4"
+      >
+        Registrarse
+      </button>
     </div>
   </form>
 </template>
 
 <script>
 export default {
-  name: 'RegisterForm'
-}
+  name: "RegisterForm",
+};
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,6 @@
 <template>
-  <main
-    class="h-screen flex flex-col justify-center items-center space-y-28"
-  >
-    <aside
-      class="flex justify-center"
-    >
+  <main class="h-screen flex flex-col justify-center items-center space-y-28">
+    <aside class="flex justify-center">
       <img
         src="src/assets/logo-bepositive.png"
         alt="BePositive logo"
@@ -12,28 +8,14 @@
       />
     </aside>
 
-    <div class="w-2/3 flex flex-col">
+    <div class="w-full text-cosmicLatte flex justify-center items-center">
       <RegisterForm />
     </div>
-  
-    <div class="w-full flex flex-col justify-center items-center">
-      <button
-        type="button"
-        class="btn bg-cosmicLatte text-intenseRose w-1/4 text-2xl rounded-full py-2"
-      >
-        <router-link 
-          class="text-2xl" 
-          :to="{ name: 'login' }"
-        >
-          Registrarse
-        </router-link>
-      </button>
+
+    <div class="w-full text-cosmicLatte flex justify-center items-center">
       <p class="text-cosmicLatte">
         ¿Ya tienes una cuenta?
-        <router-link 
-          class="text-lg font-bold" 
-          :to="{ name: 'login' }"
-        >
+        <router-link class="text-lg font-bold" :to="{ name: 'login' }">
           Inicia Sesi&oacute;n
         </router-link>
       </p>
@@ -42,12 +24,11 @@
 </template>
 
 <script>
-import RegisterForm from '../components/RegisterForm.vue'
+import RegisterForm from "../components/RegisterForm.vue";
 export default {
   name: "Register",
   components: {
     RegisterForm,
-  }
+  },
 };
 </script>
-
