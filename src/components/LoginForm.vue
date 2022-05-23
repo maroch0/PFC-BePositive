88156a9fc28689eb6466f31a5fcda9507c9087dd
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-23 18:35:35 +0200

    fix form login width

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,55 +1,31 @@
 <template>
-  <form
-    class="w-2/5 flex flex-col space-y-14 text-cosmicLatte"
-  >
-    <div
-      class="md:w-full flex flex-col space-y-4"
-    >
-      <label 
-        for="email"
-        class="text-2xl"
-      >
-        Correo Electr&oacute;nico
-      </label>
-      <input 
-        type="email" 
+  <form class="w-2/4 flex flex-col space-y-14 text-cosmicLatte">
+    <div class="md:w-full flex flex-col space-y-4">
+      <label for="email" class="text-2xl"> Correo Electr&oacute;nico </label>
+      <input
+        type="email"
         id="email"
-        required 
+        required
         class="text-xl px-1 bg-transparent border-b border-cosmicLatte outline-none"
       />
     </div>
 
-    <div
-      class="md:w-full flex flex-col space-y-4"
-    >
-      <label 
-        for="pass"
-        class="text-2xl"
-      >
-        Contrase&ntilde;a
-      </label>
-      <input 
-        type="password" 
+    <div class="md:w-full flex flex-col space-y-4">
+      <label for="pass" class="text-2xl"> Contrase&ntilde;a </label>
+      <input
+        type="password"
         id="pass"
-        required 
+        required
         class="text-xl px-1 bg-transparent border-b border-cosmicLatte outline-none"
       />
     </div>
 
-    <div
-      class="flex flex-col items-center space-y-2"
-    >
-
+    <div class="flex flex-col items-center space-y-2">
       <slot />
 
-      <p
-        class="text-cosmicLatte"
-      >
+      <p class="text-cosmicLatte">
         ¿No tienes una cuenta?
-        <router-link 
-          class="text-lg font-bold" 
-          :to="{ name: 'register' }"
-        >
+        <router-link class="text-lg font-bold" :to="{ name: 'register' }">
           Reg&iacute;strate
         </router-link>
       </p>
@@ -60,5 +36,5 @@
 <script>
 export default {
   name: "LoginForm",
-}
+};
 </script>
