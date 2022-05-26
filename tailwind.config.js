d426bf3051e60d8c5bd976d8329c4c2ac4253c9f
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-26 09:14:12 +0200

    remove unused custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'intenseRose': '#990D35',
-        'middleRose': '#BE123C',
-        'cosmicLatte': '#FFF8E8',
-        'blush': '#C87A88',
-        'irresistible': '#B0435D',
-        'amaranthPurple': '#A93251',
-        'ruby': '#A22346'
+        cosmicLatte: "#FFF8E8",
+        blush: "#C87A88",
+        amaranthPurple: "#A93251",
+        ruby: "#A22346",
       },
       width: {
-        '1/10': '12%',
+        "1/10": "12%",
       },
       borderRadius: {
-        'fifty': '50%',
-      }
+        fifty: "50%",
+      },
     },
   },
   plugins: [],
-}
+};
