c0ec9a7693c1aaba987fa5269e0507888b393dd3
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-05-29 23:49:49 +0200

    add new favicon

--- src/components/UpdateImage.vue
+++ src/components/UpdateImage.vue
@@ -27,7 +27,7 @@ export default {
   name: "UpdateImage",
   data() {
     return {
-      image: "src/assets/profile-img.png",
+      image: "../src/assets/profile-img.png",
     };
   },
   methods: {
